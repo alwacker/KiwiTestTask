e7e0c77bbd9f49ad26d5c1ecfb32aa20dbdfa7cb
Oleksandr Vaker <oleksandr.vaker@packeta.com>
2022-11-30 19:24:27 +0100

    Initial Commit

---
+++ KiwiTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KiwiTestTask
+//
+//  Created by Oleksandr Vaker on 30.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
